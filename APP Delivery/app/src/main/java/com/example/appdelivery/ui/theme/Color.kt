a6535fa9e5af43a3f26162623e2ac99e0f6911fa
andrehahn05 <andrehahn05@gmail.com>
2023-03-15 15:13:17 -0400

    colors

--- APP Delivery/app/src/main/java/com/example/appdelivery/ui/theme/Color.kt
+++ APP Delivery/app/src/main/java/com/example/appdelivery/ui/theme/Color.kt
@@ -2,14 +2,11 @@ package com.example.appdelivery.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
 val Dark40 = Color(0x8022262c)
 val Dark80 = Color(0x801b1e23)
 val Dark85 = Color(0xE821262C)
+
 
 val Red80 = Color(0xF59B0808)
 val Red40 = Color(0xF5C72F2F)
