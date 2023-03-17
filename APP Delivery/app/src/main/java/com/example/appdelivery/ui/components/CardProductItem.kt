54dc706b0695b7a74d975bf15c00e654423331b5
andrehahn05 <andrehahn05@gmail.com>
2023-03-17 11:33:05 -0400

    #

--- APP Delivery/app/src/main/java/com/example/appdelivery/ui/components/CardProductItem.kt
+++ APP Delivery/app/src/main/java/com/example/appdelivery/ui/components/CardProductItem.kt
@@ -1,24 +1,24 @@
 package com.example.appdelivery.ui.components
 
+
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme.colorScheme
-
-
-import androidx.compose.runtime.*
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.appdelivery.R
 import com.example.appdelivery.extensions.toBrazilianCurrency
 import com.example.appdelivery.model.Product
-import coil.compose.AsyncImage
 import com.example.appdelivery.sampledata.sampleProducts
 import com.example.appdelivery.ui.theme.AppDeliveryTheme
 
@@ -65,9 +65,6 @@ fun CardProductItem(
             }
         }
 }
-
-
-
 
 @Preview
 @Composable
