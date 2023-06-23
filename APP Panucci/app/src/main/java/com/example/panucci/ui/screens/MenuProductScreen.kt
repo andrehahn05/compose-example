9fd0aa7ddfc587aaafb7edba64dc1db5d5499104
andrehahn05 <andrehahn05@gmail.com>
2023-06-23 15:29:22 -0400

    MenuProductScreen

---
+++ APP Panucci/app/src/main/java/com/example/panucci/ui/screens/MenuProductScreen.kt
@@ -0,0 +1,70 @@
+package com.example.panucci.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.panucci.model.Product
+import com.example.panucci.sampledata.sampleProducts
+import com.example.panucci.ui.components.MenuProductCard
+import com.example.panucci.ui.components.MenuProductCardPreview
+import com.example.panucci.ui.theme.PanucciTheme
+import com.example.panucci.ui.theme.caveatFont
+
+@Composable
+fun MenuProductScreen(
+	modifier: Modifier = Modifier,
+	products: List<Product> = emptyList(),
+	title: String = "Menu"
+) {
+	Column(modifier.fillMaxWidth()) {
+		Surface {
+			Text(
+				text = title,
+				modifier
+					.fillMaxWidth()
+					.padding(horizontal = 8.dp),
+				fontSize = 32.sp,
+				fontFamily = caveatFont,
+				textAlign = TextAlign.Center
+			)
+		}
+		LazyColumn(
+			modifier.fillMaxSize(),
+			contentPadding = PaddingValues(16.dp),
+			verticalArrangement = Arrangement.spacedBy(8.dp)
+
+		) {
+			items(products) { p ->
+				MenuProductCard(
+					product = p
+				)
+			}
+		}
+	}
+}
+
+@Preview
+@Composable
+fun MenuProductScreenPreview() {
+	PanucciTheme {
+		Surface {
+			MenuProductScreen(
+				products = sampleProducts,
+				title = "Menu"
+			)
+		}
+	}
+}
