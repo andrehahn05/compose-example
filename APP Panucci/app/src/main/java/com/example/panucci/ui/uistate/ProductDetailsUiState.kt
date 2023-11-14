91b107faa74979cb49c1b00714e989802b199ee8
andrehahn05 <andrehahn05@gmail.com>
2023-11-13 22:45:31 -0400

    UiState

---
+++ APP Panucci/app/src/main/java/com/example/panucci/ui/uistate/ProductDetailsUiState.kt
@@ -0,0 +1,6 @@
+package com.example.panucci.ui.uistate
+
+import com.example.panucci.model.Product
+data class ProductDetailsUiState(
+    val product: Product? = null
+)
---
+++ APP Panucci/app/src/main/java/com/example/panucci/ui/viewmodels/ProductDetailsViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.panucci.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.example.panucci.dao.ProductDao
+import com.example.panucci.ui.uistate.ProductDetailsUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class ProductDetailsViewModel(
+    private val dao: ProductDao = ProductDao()
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(ProductDetailsUiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun findProductById(id: String) {
+        dao.findById(id)?.let { product ->
+            _uiState.update {
+                it.copy(product = product)
+            }
+        }
+    }
+}
